5490a9b0136133b76b6eade5c994f187bad91fb2
Jhon Fabio Cardona Martinez <fabio.cardona@coytex.com.co>
2022-10-19 07:33:39 -0500

    Creación de migraciones

--- config/app.php
+++ config/app.php
@@ -56,6 +56,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
+
     'asset_url' => env('ASSET_URL'),
 
     /*
@@ -69,7 +71,8 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    // 'timezone' => 'UTC',
+    'timezone' => 'America/Bogota',
 
     /*
     |--------------------------------------------------------------------------
