708f05a003dcff436aec394722fd16dd6168ec49
Jhon Fabio Cardona Martinez <fabio.cardona@coytex.com.co>
2022-10-19 22:23:47 -0500

    Seeders and Factories

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'Victor Peña';
+        $user->email = 'victor@gmail.com';
+        $user->password = bcrypt('admin');
+        $user->email_verified_at = '2022-05-09';
+        $user->remember_token = 'nNFhKn76Vic';
+        $user->save();
+    }
+}
